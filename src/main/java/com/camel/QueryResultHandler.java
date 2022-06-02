4f095399182e5d166c8227e7adc600c05947e761
surajoberoi15 <shiv.oberoi@knoldus.com>
2022-06-02 16:16:28 +0530

    Sql Operations Using Apache Camel and JDBC Component

---
+++ src/main/java/com/camel/QueryResultHandler.java
@@ -0,0 +1,16 @@
+package com.camel;
+
+import java.util.List;
+
+public class QueryResultHandler {
+
+    public void printResult(List list) {
+
+        for (int i = 0; i< list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
+
+
+}
